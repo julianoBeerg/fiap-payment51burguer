a000244f1b5e2c10ffa1b31d9775d0596edd783e
cjafet <cjafet07@gmail.com>
2024-12-03 00:30:20 -0300

    Add exclusions

--- src/main/java/com/fiap/burguer/Application.java
+++ src/main/java/com/fiap/burguer/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "com.fiap.burguer")
 public class Application {
 
