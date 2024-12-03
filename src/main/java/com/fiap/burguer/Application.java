a64f546b20403f58dc1fe0d98d625c18d23192f2
cjafet <cjafet07@gmail.com>
2024-12-03 00:45:10 -0300

    Remove envs

--- src/main/java/com/fiap/burguer/Application.java
+++ src/main/java/com/fiap/burguer/Application.java
@@ -2,12 +2,10 @@ package com.fiap.burguer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "com.fiap.burguer")
 public class Application {
 
